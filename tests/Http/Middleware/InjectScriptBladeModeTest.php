8d6b3692fa323b43a3d55cac192bf21f7a8d285d
DarkGhosthunter <DarkGhostHunter@gmail.com>
2022-02-08 20:55:55 -0300

    Added multiple component test

--- tests/Http/Middleware/InjectScriptBladeModeTest.php
+++ tests/Http/Middleware/InjectScriptBladeModeTest.php
@@ -14,17 +14,30 @@ class InjectScriptBladeModeTest extends TestCase
         $app->make('config')->set('poke.mode', 'blade');
     }
 
-    public function test_blade_mode_doesnt_inject_script(): void
+    public function test_doesnt_inject_script(): void
     {
         $this->get('form')->assertDontSee('start-poke-script');
     }
 
-    public function test_blade_mode_renders_once_multiple_declarations(): void
+    public function test_renders_once_on_multiple_declarations(): void
     {
         $this->get('component-multiple')
             ->assertSee('start-poke-script')
             ->assertSee(
                 Blade::render('<body><x-laragear.poke-script /></body>'), false
             );
+    }
+
+    public function test_component_rendered_forcefully_doesnt_render_twice(): void
+    {
+        $this->app->make('router')->get('component-multiple-forced', function () {
+            return Blade::render(
+                '<html><head></head><body><x-laragear.poke-script /><x-laragear.poke-script :force="true" /></body></html>'
+            );
+        });
+
+        $this->get('component-multiple-forced')->assertSee(
+            Blade::render('<html><head></head><body><x-laragear.poke-script /></body></html>'), false
+        );
     }
 }
