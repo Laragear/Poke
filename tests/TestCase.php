1968cd0cf1519024c392bfbb03813e9b4956ce68
DarkGhosthunter <DarkGhostHunter@gmail.com>
2022-02-08 20:56:05 -0300

    Minor refactoring.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,12 @@
 
 namespace Tests;
 
+use Laragear\Poke\PokeServiceProvider;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app): array
     {
-        return [
-            'Laragear\Poke\PokeServiceProvider',
-        ];
+        return [PokeServiceProvider::class];
     }
 }
