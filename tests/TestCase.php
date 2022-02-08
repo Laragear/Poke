0aab1772482ac449eafdb32a64d3c66a15855a99
DarkGhosthunter <DarkGhostHunter@gmail.com>
2022-02-08 16:33:19 -0300

    Fixed unused auth scaffold.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,10 @@ namespace Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             'Laragear\Poke\PokeServiceProvider',
-            'Laravel\Ui\UiServiceProvider' // Needed for auth scaffolding
         ];
     }
 }
